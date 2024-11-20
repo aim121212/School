b5d87f5f70d39babd0853e74fce2389f2d035a34
Daniel <daniel@ASUS-Debian.lan>
2024-11-20 11:34:04 -0700

    Pointers update

--- In-class Code/Pointers.c
+++ In-class Code/Pointers.c
@@ -68,6 +68,8 @@ void main() {
 
 */
 
+/*
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -111,5 +113,57 @@ void main() {
 
     else {
         printf("Program will now exit.\n");
+    }
+}
+
+*/
+
+//November 20th
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main() {
+
+    srand (time(NULL));
+
+    int a[5];
+    int * pointer;
+
+    for (int i = 0; i < 5; i++) {
+    a[i] = rand() % 7 + 1;
+    }
+
+    printf("Values:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    printf("\nResult using &a is %d.\n", &a);
+    printf("Result using a is %d.\n", a);
+    printf("Result using &a[0] is %d.\n", &a[0]);
+
+    printf("\nResult using &a + 1 is %d.\n", &a + 1);
+    printf("Result using a + 1 is %d.\n", a + 1);
+    printf("Result using &a[0] + 1 is %d.\n", &a[0] + 1);
+
+    printf("\nUsiing pointers:\n");
+    for (int i = 0; i < 5; i++) {
+
+    pointer = &a[i];
+
+        printf("\nCurrent index is %d.\n", i);
+        printf("Result using value is %d.\n", a[i]);
+        printf("Result using pointer is %d.\n", *pointer);
+        printf("Address: %x\n", pointer);
+    }
+
+    printf("\nProfessor's version:");
+    for (int i = 0; i < 5; i++) {
+
+        pointer = &a[i];
+
+        printf("\nThe index %d, value is %d, the address in hexadecimal is %x, the address in decimal is %d.", i, *pointer, pointer, pointer);
     }
 }
